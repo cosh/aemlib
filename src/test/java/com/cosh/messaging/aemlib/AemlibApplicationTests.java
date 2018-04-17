2cfa188508a214bef1d876d17c421b8c16848de6
Henning Rauch <Henning.Rauch@Microsoft.com>
2018-04-17 10:03:52 +0200

    initial commit

---
+++ src/test/java/com/cosh/messaging/aemlib/AemlibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cosh.messaging.aemlib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AemlibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
